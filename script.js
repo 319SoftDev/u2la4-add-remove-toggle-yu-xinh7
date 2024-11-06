823a1f1dda8dc19bde3c20022f3f520164a6b564
yu-xinh7 <yuxinh2@nycstudents.net>
2024-11-06 10:25:54 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+const heading = document.querySelector("#heading");
+
+
+const changeFontColor = ()  => {
+    heading.classList.add("blue-text");
+}
+
+heading.addEventListener("click", changeFontColor);
+
+
+//  subheading 
+const subheading = document.querySelector("#subheading");
+
+const changeFontColors = () => {
+    subheading.classList.toggle("blue-text");
+}
+
+subheading.addEventListener("click", changeFontColors);
+
+//  Reply buttons
+const replyButton = document.querySelector ("#reply-button");
+const replyMessage = document.querySelector ("#reply-message");
+
+const openReplyMessage = () => {
+    replyMessage.classList.remove("hidden");
+}
+
+replyButton.addEventListener("click", openReplyMessage);
+
+// Send d& cancel buttons hide text box ( and hidden )
+
+const sendButton = document.querySelector ("#send-button");
+const cancelButton = document.querySelector ("#cancel-button");
+
+const closeReplyMessage = () => {
+    replyMessage.classList.add("hidden");
+}
+
+cancelButton.addEventListener("click", closeReplyMessage );
+sendButton.addEventListener("click", closeReplyMessage );
+
+// medium activities
+const inbox = document.querySelector("#inbox");
+const imbox = () => {
+    
+}
