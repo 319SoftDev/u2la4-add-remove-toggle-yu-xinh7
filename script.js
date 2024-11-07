bf44d44d932e045433c2887007ea3dfb20dfd9d5
yu-xinh7 <yuxinh2@nycstudents.net>
2024-11-07 10:34:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -41,8 +41,19 @@ sendButton.addEventListener("click", closeReplyMessage );
 
 // medium activities
 const isRead = document.querySelector("#is-read");
-const inbox = () => {
-    Read.classlist.add("isRead");
+const inboxMessage = document.querySelector("inbox-message");
+const markUnread = document.querySelector("#mark-button");
+
+const open = () => {
+    inbox.classlist.add("isRead");
+    inboxMessage.classList.remove("hidden");
+    markUnread.classList.remove("#hidden")
 }
 
-isRead.addEventListener("click", isRead );
+open.addEventListener("click", isRead);
+open.addEventListener("click", inboxMessage);
+open.addEventListener("click", markUnread);
+
+const close = () => {
+    
+}
